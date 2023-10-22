e37ddc7e0704ef9b0b1dd2cf7b44dbe24f93c6c4
MysteriousRomeo <roy.ayan.roy@gmail.com>
2023-10-22 16:09:44 +0100

    week7homework

---
+++ Ayan07/src/javaWeek2/Test01.java
@@ -0,0 +1,12 @@
+/*
+1. Write a Java programme using the following steps.
+1.1 Declare two instance variables.
+1.2 Declare one instance method.
+1.3 Call both instance variables into the instance method inside the print statement.
+1.4 Declare the Main method.
+1.5 Call the above instance method into the Main method and run the programme.
+*/
+package javaWeek2;
+
+public class Test01 {
+}
